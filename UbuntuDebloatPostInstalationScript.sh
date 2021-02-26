f7636cce52db152c214ca29e99026b53a2b0ea0b
Marques576 <tomasm576@gmail.com>
2021-02-26 20:24:14 +0000

    Update UbuntuDebloatPostInstalationScript.sh

--- UbuntuDebloatPostInstalationScript.sh
+++ UbuntuDebloatPostInstalationScript.sh
@@ -14,7 +14,6 @@ sudo apt purge gnome-sudoku -y
 sudo apt purge gnome-mines -y
 sudo apt purge gnome-mahjongg -y
 echo "Removed all the gnome games"
-sudo apt install gnome-software -y
 sudo apt purge gnome-software-plugin-snap -y
 echo "Now you have a REAL SOFTWARE CENTER!"
 sudo apt purge thunderbird
@@ -25,10 +24,8 @@ sudo apt purge remmina -y
 echo "remmina removed"
 sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
 echo "Now ubuntu is updated!"
-sudo apt install gnome-tweaks -y
 echo "Now you can customise GNOME"
 sudo apt purge evince -y
 echo "evince/documents removed"
 sudo apt purge ubuntu-report popularity-contest apport whoopsie -y
 echo "Yay ubuntu stoped spying on you!"
-sudo apt install vlc flasmeshot vim -y
